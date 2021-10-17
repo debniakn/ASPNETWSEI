174de0389c569df8d7812be6707b4d0b1afe06cd
maciejdebniak <maciej.debniak@gmail.com>
2021-10-17 10:10:07 +0200

    init

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspnetWSEI.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspnetWSEI.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Name = "English" };
+            return View(firstBook);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AspnetWSEI.Models
+{
+    public class Book
+    { 
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+    }
+}
