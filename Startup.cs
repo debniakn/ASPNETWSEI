f779aa57e12b3b068eb60d11b9d17bdd2114cc21
maciejdebniak <maciej.debniak@gmail.com>
2021-10-17 10:40:07 +0200

    added attribute-based route

--- Startup.cs
+++ Startup.cs
@@ -60,13 +60,7 @@ namespace AspnetWSEI
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "BooksByReleasedDay",
-                    pattern: "/books/released/{year}/{month}",
-                    defaults: new { controller = "Books", action = "ByReleaseDate" },
-                    new {year = @"\d{4}", month = @"\d{2}"}
-                    );
+            { 
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
